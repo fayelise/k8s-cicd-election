4587660528cc8c647e1afb93bdbcd49ef5666c9f
fayelise <faye.elise@uam.edu.sn>
2026-08-30 18:39:43 +0000

    Update demo API message

--- demo-api/app.py
+++ demo-api/app.py
@@ -11,7 +11,7 @@ VERSION = os.environ.get("APP_VERSION", "dev")
 
 @app.route("/")
 def home():
-    return jsonify(message="Hello from the CI/CD demo API!", version=VERSION)
+    return jsonify(message="Hello from the CI/CD demo API! v2", version=VERSION)
 
 
 @app.route("/version")
